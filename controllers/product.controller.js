e08af2e98cf269aae2d6d6f04f3a77745d4ee05a
vinicio.covalski <vinicio.covalski@gmail.com>
2022-04-03 20:10:05 -0300

    Store API inicial

---
+++ controllers/product.controller.js
@@ -0,0 +1,75 @@
+import ProductService from "../services/product.service.js";
+
+async function createProduct(req, res, next) {
+  try {
+    let product = req.body;
+
+    if (
+      !product.name ||
+      !product.description ||
+      !product.value ||
+      !product.stock ||
+      !product.supplier_id
+    ) {
+      throw new Error("Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios.");
+    }
+    product = await ProductService.createProduct(product);
+    res.send(product);
+    global.logger.info(`POST /product - ${JSON.stringify(product)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getProducts(req, res, next) {
+  try {
+    res.send(await ProductService.getProducts());
+    global.logger.info("GET /product");
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getProduct(req, res, next) {
+  try {
+    res.send(await ProductService.getProduct(req.params.id));
+    global.logger.info("GET /product ID");
+  } catch (err) {
+    next(err);
+  }
+}
+
+
+async function deleteProduct(req, res, next) {
+  try {
+   await ProductService.deleteProduct(req.params.id);
+    global.logger.info("DELETE /product ID");
+    res.end();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  try {
+    let product = req.body;
+
+    if (
+      !product.product_id ||
+      !product.name ||
+      !product.description ||
+      !product.value ||
+      !product.stock ||
+      !product.supplier_id
+    ) {
+      throw new Error("Product ID, Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios.");
+    }
+    product = await ProductService.updateProduct(product);
+    res.send(product);
+    global.logger.info(`PUT /product - ${JSON.stringify(product)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default { createProduct, getProducts, getProduct, deleteProduct,updateProduct};

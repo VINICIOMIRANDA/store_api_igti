478a39d1d0721ec8c27c54577070577a1a59fcc2
vinicio.covalski <vinicio.covalski@gmail.com>
2022-04-08 00:38:55 -0300

    SEQUELIZE PRODUCT

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,7 +11,9 @@ async function createProduct(req, res, next) {
       !product.stock ||
       !product.supplierId
     ) {
-      throw new Error("Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios.");
+      throw new Error(
+        "Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios."
+      );
     }
     product = await ProductService.createProduct(product);
     res.send(product);
@@ -39,10 +41,9 @@ async function getProduct(req, res, next) {
   }
 }
 
-
 async function deleteProduct(req, res, next) {
   try {
-   await ProductService.deleteProduct(req.params.id);
+    await ProductService.deleteProduct(req.params.id);
     global.logger.info("DELETE /product ID");
     res.end();
   } catch (err) {
@@ -62,7 +63,9 @@ async function updateProduct(req, res, next) {
       !product.stock ||
       !product.supplierId
     ) {
-      throw new Error("Product ID, Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios.");
+      throw new Error(
+        "Product ID, Name, Descrição, Valor, Estoque, Codigo do Fornecedor e Address são obrigatórios."
+      );
     }
     product = await ProductService.updateProduct(product);
     res.send(product);
@@ -72,4 +75,95 @@ async function updateProduct(req, res, next) {
   }
 }
 
-export default { createProduct, getProducts, getProduct, deleteProduct,updateProduct};
+async function createProductInfo(req, res, next) {
+  try {
+    let productInfo = req.body;
+    if (!productInfo.productId) {
+      throw new Error("product Id é obrigatorio");
+    }
+    await ProductService.createProductInfo(productInfo);
+    res.end();
+    global.logger.info(`POST /product/info - ${JSON.stringify(productInfo)}`);
+
+    // res.send(productInfo)
+  } catch (err) {
+    next(err);
+  }
+}
+
+
+async function updateProductInfo(req, res, next) {
+  try {
+    let productInfo = req.body;
+    if (!productInfo.productId) {
+      throw new Error("product Id é obrigatorio");
+    }
+    await ProductService.updateProductInfo(productInfo);
+    res.end();
+    global.logger.info(`PUT /product/info - ${JSON.stringify(productInfo)}`);
+
+    // res.send(productInfo)
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function createReview(req, res, next){
+  try {
+    let params = req.body;
+    if (!params.productId || !params.review){
+    
+      throw new Error("Product Id e Review são obrigatorios");
+     
+    }
+     await ProductService.createReview(params.review, params.productId );
+    res.end();
+    global.logger.info(`POST CREATE REVIEW /product/review`);
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteReview(req, res, next){
+  try {
+    
+     await ProductService.deleteReview(req.params.id,req.params.index );
+    res.end();
+    global.logger.info(`DELETE REVIEW /product/${req.params.id}/review/${req.params.index}`);
+
+  } catch (err) {
+    next(err);
+  }
+}
+async function getProductsInfo(req, res, next) {
+  try {
+    res.send(await ProductService.getProductsInfo());
+    global.logger.info("GET /product/info ");
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteProductInfo(req, res, next) {
+  try {
+    res.send(await ProductService.deleteProductInfo(req.params.id));
+    global.logger.info("DELETE /product/info ");
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default {
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+  createProductInfo,
+  updateProductInfo,
+  createReview,
+  deleteReview,
+  getProductsInfo,
+  deleteProductInfo
+};
